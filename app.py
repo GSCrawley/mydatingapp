4587a0853cdb6c3431c13ec7b6797d7728dc8143
GSCrawley <gscrawley@gmail.com>
2020-01-02 13:09:33 -0800

    slowly but surely

--- app.py
+++ app.py
@@ -10,8 +10,21 @@ def home():
     "Show Homepage"
     return render_template('home.html', home=home)
 
+@app.route('/about')
+def about():
+  return render_template('about.html', about=about)
 
+@app.route('/signup')
+def signup():
+  return render_template('signup.html', signup=signup)
 
+@app.route('/login')
+def login():
+  return render_template('login.html', login=login)
+
+@app.route('/vidchat')
+def vidchat():
+  return render_template('vidchat.html', vidchat=vidchat)
 
 if __name__ == '__main__':
   app.run(debug=True, host='0.0.0.0', port=os.environ.get('PORT', 5000))
